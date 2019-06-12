7cd222af42d9805f7ee4a0f68862f3dd20491abc
Tiv0w <t.theomeyer@gmail.com>
2019-06-13 01:05:15 +0200

    :lipstick: added transparency
    new toggle-transparency and transparency functionsfor transparency support

--- elisp/base-functions.el
+++ elisp/base-functions.el
@@ -19,7 +19,7 @@
     (neotree-toggle)
     (linum-mode nil)
     (if project-dir
-	(if (neo-global--window-exists-p)
+        (if (neo-global--window-exists-p)
             (progn
               (neotree-dir project-dir)
               (neotree-find file-name)))
@@ -29,5 +29,26 @@
   "Disable current theme before loading new one."
   (mapcar #'disable-theme custom-enabled-themes))
 (advice-add 'load-theme :before #'load-theme--disable-old-theme)
+
+
+;; Transparency support ?
+;; Works well on Emacs 26.2 built with toolkit=lucid on Manjaro Gnome
+(defun transparency (value)
+  "Sets the transparency of the frame window. 0=transparent/100=opaque"
+  (interactive "nTransparency Value 0 - 100 opaque:")
+  (set-frame-parameter (selected-frame) 'alpha value))
+
+(defun toggle-transparency ()
+  (interactive)
+  (let ((alpha (frame-parameter nil 'alpha)))
+    (set-frame-parameter
+     nil 'alpha
+     (if (eql (cond ((numberp alpha) alpha)
+                    ((numberp (cdr alpha)) (cdr alpha))
+                    ;; Also handle undocumented (<active> <inactive>) form.
+                    ((numberp (cadr alpha)) (cadr alpha)))
+              100)
+         '(85 . 50) '(100 . 100)))))
+
 
 (provide 'base-functions)
