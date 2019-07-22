bc4f781052bdb57b8826266ec79df05bf86db3fc
Tiv0w <t.theomeyer@gmail.com>
2019-07-22 11:42:46 +0200

    :children_crossing: added new keybind for magit-status

--- elisp/xah-fly-keys-setup.el
+++ elisp/xah-fly-keys-setup.el
@@ -6,22 +6,29 @@
 (xah-fly-keys-set-layout "qwerty")
 
 ;; custom keybinds
+
+;; main key map
+;; doesn't work well
 (define-key xah-fly-key-map (kbd "n") 'isearch-forward-regexp)
 (define-key xah-fly-key-map (kbd "a") 'counsel-M-x)
 
+;; leader key map
+
 ;;;;;;;;;;;;;;;;;;;;
 ;; too much power ;;
-;;;;;;;;;;;;;;;;;;;;
 (define-key xah-fly-leader-key-map (kbd "m") 'major-mode-hydra)
 ;;;;;;;;;;;;;;;;;;;;
-
+(define-key xah-fly-leader-key-map (kbd "7") 'magit-status)
 (define-key xah-fly-leader-key-map (kbd "f") 'counsel-switch-buffer)
+(define-key xah-fly-leader-key-map (kbd "p") 'multiple-cursors-hydra/body)
 (define-key xah-fly-leader-key-map (kbd "u") 'delete-region)
 (define-key xah-fly-leader-key-map (kbd "z") 'avy-goto-char)
 
+;; hydra key map
 (define-key xah-fly-hydra-keymap (kbd "b") 'buffer-move-hydra/body)
-(define-key xah-fly-hydra-keymap (kbd "j") 'multiple-cursors-hydra/body)
+(define-key xah-fly-hydra-keymap (kbd "SPC") 'multiple-cursors-hydra/body)
 
+;; dot key map
 ;; (define-key xah-fly-dot-keymap (kbd "a") 'flyspell-correct-at-point)
 (define-key xah-fly-dot-keymap (kbd "SPC") 'projectile-command-map)
 (define-key xah-fly-dot-keymap (kbd "b") 'revert-buffer)
@@ -34,7 +41,9 @@
 (define-key xah-fly-dot-keymap (kbd "t") 'untabify)
 (define-key xah-fly-dot-keymap (kbd "u") 'dumb-jump-back)
 
+;; w key map
 (define-key xah-fly-w-keymap (kbd "x") 'server-shutdown)
+
 
 ;; send C-g when pressing ESC
 (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
