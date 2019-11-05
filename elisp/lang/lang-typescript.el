820520c980f4b635705c6a98710c0e824fe00299
Tiv0w <t.theomeyer@gmail.com>
2019-11-05 21:40:34 +0100

    :recycle: refactored lang-typescript

--- elisp/lang/lang-typescript.el
+++ elisp/lang/lang-typescript.el
@@ -2,25 +2,20 @@
 ;; A setup for TypeScript support
 
 (use-package typescript-mode
+  :mode-hydra
+  ((:title "Typescript-mode" :color red :quit-key "q")
+   ("Navigation"
+    (("m" tide-jump-to-definition "jump to def")
+     ("," tide-jump-back "jump back")
+     ("." tide-jump-to-implementation "jump to impl"))
+    "Editing"
+    (("s" tide-rename-symbol "rename symbol" :color blue)
+     ("d" tide-rename-file "rename file" :color blue)
+     ("f" tide-fix "fix" :color blue)
+     ("r" tide-refactor "refactor" :color blue)
+     ("s" tide-organize-imports "select" :color blue)
+     ("t" tide-add-tslint-disable-next-line "tslint disable" :color blue))))
   :config
-  (use-package add-node-modules-path
-    :hook typescript-mode)
-  (use-package flycheck
-    :hook (typescript-mode . flycheck-mode)
-    :after add-node-modules-path
-    :config
-    (setq-default flycheck-disabled-checkers
-                  (append flycheck-disabled-checkers
-                          '(javascript-jshint)))
-    ;; we prefer tslint to eslint, hence the order
-    (cond ((executable-find "tslint")
-           (flycheck-add-mode 'typescript-tslint 'typescript-mode)
-           (flycheck-select-checker 'typescript-tslint))
-          ((executable-find "eslint")
-           (flycheck-add-mode 'javascript-eslint 'typescript-mode)
-           (flycheck-select-checker 'javascript-eslint)))
-    (flycheck-mode))
-
   (set-pretty-symbols! 'typescript-mode
     ;; Functional
     :def "function"
@@ -37,6 +32,34 @@
     :and "&&" :or "||"
     :for "for"
     :return "return" :yield "import"))
+
+(use-package add-node-modules-path
+  :hook typescript-mode)
+
+(use-package flycheck
+  :hook (typescript-mode . flycheck-mode)
+  :after (typescript-mode add-node-modules-path)
+  :config
+  (setq-default flycheck-disabled-checkers
+		(append flycheck-disabled-checkers
+			'(javascript-jshint)))
+  ;; we prefer tslint to eslint, hence the order
+  (cond ;; ((executable-find "tslint")
+	;;  (flycheck-add-mode 'typescript-tslint 'typescript-mode)
+	;;  (flycheck-select-checker 'typescript-tslint))
+	((executable-find "eslint")
+	 (flycheck-add-mode 'javascript-eslint 'typescript-mode)
+	 (flycheck-select-checker 'javascript-eslint)))
+  (flycheck-mode))
+
+(use-package tide
+  :ensure t
+  :after (typescript-mode company flycheck add-node-modules-path)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save))
+  :config
+  (eldoc-mode +1))
 
 (use-package eslintd-fix
   :hook (typescript-mode . eslintd-fix-mode))
