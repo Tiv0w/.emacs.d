21151b0932dc5b07f0e05d6b9badd74b49d47ec8
Tiv0w <t.theomeyer@gmail.com>
2020-01-13 11:25:08 +0100

    :zap: updated python configuration

--- elisp/lang/lang-python.el
+++ elisp/lang/lang-python.el
@@ -4,20 +4,18 @@
 
 ;;; Code:
 
-(use-package python
-  :mode ("\\.py" . python-mode)
+(use-package elpy
+  :mode ("\\.py$" . python-mode)
+  :hook python-mode
   :config
-  (use-package elpy
-    :init
-    (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
-    :config
-    (setq elpy-rpc-backend "jedi")
-    ;; (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
-    ;;flycheck-python-flake8-executable "/usr/local/bin/flake8"
-    :bind (:map elpy-mode-map
-	      ("M-." . elpy-goto-definition)
-	      ("M-," . pop-tag-mark)))
-  (elpy-enable))
+  (setq elpy-rpc-virtualenv-path 'current)
+  (setq elpy-rpc-backend "jedi")
+  ;; (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
+  ;;flycheck-python-flake8-executable "/usr/local/bin/flake8"
+  (elpy-enable)
+  :bind (:map elpy-mode-map
+              ("M-." . elpy-goto-definition)
+              ("M-," . pop-tag-mark)))
 
 (use-package pip-requirements
   :config
@@ -26,63 +24,32 @@
 (use-package py-autopep8)
 
 
-(use-package pyenv-mode
-  :if
-  (executable-find "pyenv")
-  :init
-  (add-to-list 'exec-path "~/.pyenv/shims")
-  (setenv "WORKON_HOME" "~/.pyenv/versions/")
-  :config
-  (pyenv-mode)
-  :bind
-  ("C-x p e" . pyenv-activate-current-project))
-
-(defun pyenv-init()
-  (setq global-pyenv (replace-regexp-in-string "\n" "" (shell-command-to-string "pyenv global")))
-  (message (concat "Setting pyenv version to " global-pyenv))
-  (pyenv-mode-set global-pyenv)
-  (defvar pyenv-current-version nil global-pyenv))
-
-(defun pyenv-activate-current-project ()
-  "Automatically activates pyenv version if .python-version file exists."
-  (interactive)
-  (f-traverse-upwards
-   (lambda (path)
-     (message path)
-     (let ((pyenv-version-path (f-expand ".python-version" path)))
-       (if (f-exists? pyenv-version-path)
-          (progn
-            (setq pyenv-current-version (s-trim (f-read-text pyenv-version-path 'utf-8)))
-            (pyenv-mode-set pyenv-current-version)
-            (pyvenv-workon pyenv-current-version)
-            (message (concat "Setting virtualenv to " pyenv-current-version))))))))
-
-(add-hook 'after-init-hook 'pyenv-init)
-(add-hook 'projectile-after-switch-project-hook 'pyenv-activate-current-project)
+(use-package pyvenv
+  :commands pyvenv-activate)
 
 
 (set-pretty-symbols! 'python-mode
-    ;; Functional
-    :def "def"
-    :lambda "lambda"
-    ;; Types
-    :null "None"
-    :true "True"
-    :false "False"
-    :int "int"
-    :str "str"
-    :float "float"
-    :bool "bool"
-    :tuple "tuple"
-    ;; Flow
-    :not "not"
-    :in "in"
-    :not-in "not in"
-    :and "and"
-    :or "or"
-    :for "for"
-    :return "return"
-    :yield "yield")
+  ;; Functional
+  :def "def"
+  :lambda "lambda"
+  ;; Types
+  :null "None"
+  :true "True"
+  :false "False"
+  :int "int"
+  :str "str"
+  :float "float"
+  :bool "bool"
+  :tuple "tuple"
+  ;; Flow
+  :not "not"
+  :in "in"
+  :not-in "not in"
+  :and "and"
+  :or "or"
+  :for "for"
+  :return "return"
+  :yield "yield")
 
 (provide 'lang-python)
 ;;; base-python.el ends here
