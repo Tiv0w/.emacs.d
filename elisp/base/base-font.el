2fbaec0abc89b40888a3f587150c47ed0c61fba2
Tiv0w <t.theomeyer@gmail.com>
2020-06-20 08:15:23 +0200

    :bug: fix variable font height for laptop and desktop

--- elisp/base/base-font.el
+++ elisp/base/base-font.el
@@ -7,9 +7,9 @@
 
 (defvar-local font-height (if (getenv "IS_LAPTOP") 120 100))
 
-(set-face-attribute 'default nil :family "Office Code Pro D" :height 'font-height)
+(set-face-attribute 'default nil :family "Office Code Pro D" :height font-height)
 (set-face-attribute 'fixed-pitch nil :family "Office Code Pro D")
-(set-face-attribute 'variable-pitch nil :family "Google Sans" :height 'font-height)
+(set-face-attribute 'variable-pitch nil :family "Google Sans" :height font-height)
 
 (add-hook 'after-make-frame-functions
 	  (lambda (frame)
