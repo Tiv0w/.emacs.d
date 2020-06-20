5466550c7a5c4591a4f58e565e344779ee8b19cf
Tiv0w <t.theomeyer@gmail.com>
2020-06-20 08:18:09 +0200

    :sparkles: added rust major mode hydra

--- elisp/lang/lang-rust.el
+++ elisp/lang/lang-rust.el
@@ -10,12 +10,26 @@
   :bind (:map rust-mode-map
               (("C-c C-t" . racer-describe)))
   :config
+  (setq indent-tabs-mode nil)
   (add-hook 'rust-mode-hook 'flycheck-mode)
   ;; format rust buffers on save using rustfmt
   (add-hook 'before-save-hook
             (lambda ()
               (when (eq major-mode 'rust-mode)
-                (rust-format-buffer)))))
+                (rust-format-buffer))))
+  :mode-hydra
+  ((:quit-key "q")
+   ("Rust"
+    (("w" rust-run-clippy "clippy")
+     ("e" rust-compile "compile")
+     ("r" rust-run "run")
+     ("t" rust-test "test")
+     ("p" rust-promote-module-into-dir "promote module"))
+    "Cargo"
+    (("u" cargo-process-doc-open "cargo doc open"))
+    "Racer"
+    (("d" racer-describe "doc")
+     ("s" racer-find-definition "find def")))))
 
 
 (use-package flycheck-rust
