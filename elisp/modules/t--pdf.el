3151fa1e422441280ed898c5756e98e7d8d5afb3
Tiv0w <t.theomeyer@gmail.com>
2020-11-10 17:25:41 +0100

    :sparkles: Add a major-mode-hydra to pdf-view-mode

--- elisp/modules/t--pdf.el
+++ elisp/modules/t--pdf.el
@@ -6,7 +6,13 @@
 
 (use-package pdf-tools
   :mode ("\\.[pP][dD][fF]\\'" . pdf-view-mode)
-  :magic ("%PDF" . pdf-view-mode))
+  :magic ("%PDF" . pdf-view-mode)
+  :mode-hydra
+  (pdf-view-mode
+   (:color blue :quit-key "q")
+   ("PDF"
+    (("n" pdf-view-midnight-minor-mode "midnight")
+     ("c" pdf-view-kill-ring-save "copy")))))
 
 (use-package org-noter
   :disabled
