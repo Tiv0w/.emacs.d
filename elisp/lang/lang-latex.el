99635440dbbc094f1fed0cd5df66cd55c7e81a36
Tiv0w <t.theomeyer@gmail.com>
2021-02-09 10:04:31 +0100

    :recycle: Refactor lang-latex, now fully functional

--- elisp/lang/lang-latex.el
+++ elisp/lang/lang-latex.el
@@ -1,23 +1,25 @@
 ;;; elisp/lang/lang-latex.el -*- lexical-binding: t; -*-
 
 ;; AUCTeX configuration
-
-(add-to-list 'auto-mode-alist '("\\.tex\\'" . LaTeX-mode))
-
-(setq TeX-auto-save t
+(use-package tex
+  :ensure auctex
+  :mode ("\\.tex\\'" . LaTeX-mode)
+  :config
+  (setq TeX-auto-save t
       TeX-parse-self t
       TeX-close-quote ""
       TeX-open-quote ""
       TeX-PDF-mode t)
-(setq-default TeX-master nil)
+  (setq-default TeX-master nil)
+  (add-to-list 'TeX-view-program-list '("preview-pane" latex-preview-pane-mode))
+  (add-to-list 'TeX-view-program-selection '(output-pdf "PDF Tools"))
+  ;; (when (executable-find "zathura")
+  ;;        (add-to-list 'TeX-view-program-selection '(output-pdf "Zathura"))
+  (add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer))
 
-(add-hook 'LaTeX-mode-hook (lambda () (setq ispell-parser 'tex)))
 
-(add-to-list 'TeX-view-program-list '("preview-pane" latex-preview-pane-mode))
-(add-to-list 'TeX-view-program-selection '(output-pdf "PDF Tools"))
-;; (when (executable-find "zathura")
-;;        (add-to-list 'TeX-view-program-selection '(output-pdf "Zathura"))
-(add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer)
+;; (add-hook 'LaTeX-mode-hook (lambda () (setq ispell-parser 'tex)))
+
 
 (use-package flyspell
   :hook (LaTeX-mode . flyspell-mode)
