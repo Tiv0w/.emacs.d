90ac7175d0100f47c77252812ecbca446d1a92d5
Tiv0w <t.theomeyer@gmail.com>
2022-02-03 15:49:45 +0100

    Add all-the-icons-ivy-rich

--- elisp/modules/t--ivy.el
+++ elisp/modules/t--ivy.el
@@ -33,6 +33,9 @@
 				(t . ivy--regex-plus)))
   (define-key read-expression-map (kbd "C-r") 'counsel-expression-history))
 
+(use-package all-the-icons-ivy-rich
+  :init (all-the-icons-ivy-rich-mode 1))
+
 (use-package ivy-rich
   :after ivy
   :config
