cd66419dd09434ad583d0cc91953f97c597bd206
Tiv0w <t.theomeyer@gmail.com>
2022-04-08 22:42:13 +0200

    Make lang-ruby more fully featured

--- elisp/lang/lang-ruby.el
+++ elisp/lang/lang-ruby.el
@@ -1,13 +1,33 @@
 ;;; elisp/lang/lang-ruby.el -*- lexical-binding: t; -*-
 
-(use-package enh-ruby-mode
-  :mode
-  (("\\.rb\\'" . ruby-mode)))
+(use-package lsp-mode
+  :hook (ruby-mode . lsp)
+  :config
+  (setq lsp-solargraph-use-bundler t
+	lsp-sorbet-use-bundler t
+	lsp-steep-use-bundler t
+	lsp-typeprof-use-bundler t))
+
+(use-package yard-mode
+  :hook ruby-mode)
 
 (use-package robe
+  :hook (ruby-mode . robe-mode)
+  :after yard-mode
   :config
   (push 'company-robe company-backends))
 
-(use-package rinari)
+(use-package rinari
+  :hook (ruby-mode . rinari-minor-mode))
+
+(use-package rubocop
+  :hook (ruby-mode . rubocop-mode))
+
+(use-package bundler
+  :hook (ruby-mode . bundler-mode))
+
+(use-package rbenv
+  :config
+  (add-to-list 'exec-path (expand-file-name "shims" rbenv-installation-dir)))
 
 (provide 'lang-ruby)
