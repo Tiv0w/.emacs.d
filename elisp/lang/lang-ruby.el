9e97e6f3299dc15e8b41b26e2bebaa7cff0c7421
Tiv0w <t.theomeyer@gmail.com>
2022-04-12 23:52:44 +0200

    Add major-mode hydra to ruby-mode

--- elisp/lang/lang-ruby.el
+++ elisp/lang/lang-ruby.el
@@ -1,5 +1,31 @@
 ;;; elisp/lang/lang-ruby.el -*- lexical-binding: t; -*-
 
+;;; Ruby configuration
+;; Uses LSP with the solargraph server
+;; If using bundler, add solargraph as a dev dependency
+;; and run `yard gems' in the root of the repo.
+;;
+;; For robe, add pry, pry-doc and webrick as dev dependencies.
+
+(use-package ruby-mode
+  :ensure nil ; an Emacs built-in
+  :mode-hydra
+  ((:title "Ruby" :color blue :quit-key "q")
+   ("Essential"
+    (("a" robe-start "start robe")
+     ("m" robe-jump "jump to def")
+     ("d" robe-doc "doc"))
+    "Eval"
+    (("ee" ruby-send-last-stmt "last statement")
+     ("ed" ruby-send-definition "def")
+     ("eb" ruby-send-buffer "buffer"))
+    "Rubocop"
+    (("la" rubocop-autocorrect-current-file "autocorrect")
+     ("lc" rubocop-check-current-file "check")
+     ("lf" rubocop-format-current-file "format"))
+    "Misc"
+    (("{" ruby-toggle-block "block -> {")))))
+
 (use-package lsp-mode
   :hook (ruby-mode . lsp)
   :config
@@ -10,7 +36,7 @@
 
 (use-package rbenv
   :config
-  (add-to-list 'exec-path (expand-file-name "shims" rbenv-installation-dir)))
+  (add-to-list 'exec-path (expand-file-name "shims/" rbenv-installation-dir)))
 
 (use-package yard-mode
   :hook ruby-mode)
@@ -22,7 +48,7 @@
   (push 'company-robe company-backends))
 
 (use-package bundler
-  :hook (ruby-mode . bundler-mode))
+  :after ruby-mode)
 
 (use-package rinari
   :hook (ruby-mode . rinari-minor-mode))
