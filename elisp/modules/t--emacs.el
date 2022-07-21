b5dfe9e96f570bcc6b45d2b5e470c6a4b3b49bc3
Tiv0w <t.theomeyer@gmail.com>
2022-07-21 18:22:34 +0200

    Add ANSI color parsing support in compilation-mode

--- elisp/modules/t--emacs.el
+++ elisp/modules/t--emacs.el
@@ -96,4 +96,17 @@
   :config
   (persistent-scratch-setup-default))
 
+
+;;; Taken from
+;;; endlessparentheses.com/ansi-colors-in-the-compilation-buffer-output.html
+;; Fix ANSI colors in compilation-mode
+(require 'ansi-color)
+(defun endless/colorize-compilation ()
+  "Colorize from `compilation-filter-start' to `point'."
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region
+     compilation-filter-start (point))))
+
+(add-hook 'compilation-filter-hook #'endless/colorize-compilation)
+
 (provide 't--emacs)
