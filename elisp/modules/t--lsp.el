60afcb567f4662f28a5d6eef2e9596ea9a29ada7
Tiv0w <t.theomeyer@gmail.com>
2023-12-19 18:24:07 +0100

    Set up lsp-ui cleanly for once

--- elisp/modules/t--lsp.el
+++ elisp/modules/t--lsp.el
@@ -16,6 +16,20 @@
    ;; Makes LSP shutdown the server when all buffers in the project are closed.
    lsp-keep-workspace-alive nil))
 
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :config
+  (setq lsp-ui-sideline-diagnostic-max-lines 2
+        lsp-ui-sideline-show-hover nil
+        lsp-ui-sideline-show-diagnostics t
+	lsp-ui-peek-enable t
+	lsp-ui-doc-max-height 8
+	lsp-ui-doc-max-width 72 ; default 150
+	lsp-ui-doc-delay 0.75	; default 0.2
+	lsp-ui-doc-show-with-mouse nil
+	lsp-ui-sideline-ignore-duplicate t
+	lsp-ui-sideline-actions-icon lsp-ui-sideline-actions-icon-default))
+
 ;; Use the Debug Adapter Protocol for running tests and debugging
 (use-package dap-mode
   :hook
