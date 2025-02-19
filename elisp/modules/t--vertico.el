443799b6c32ea2b14a7fbc62973e01c74781119b
Tiv0w <t.theomeyer@gmail.com>
2025-02-19 17:43:31 +0100

    Small cleanup

--- elisp/modules/t--vertico.el
+++ elisp/modules/t--vertico.el
@@ -101,41 +101,4 @@
   :commands (wgrep-change-to-wgrep-mode)
   :config (setq wgrep-auto-save-buffer t))
 
-;; (use-package counsel
-;;   :commands (counsel-M-x find-file counsel-find-file)
-;;   :bind
-;;   ("M-x" . counsel-M-x)
-;;   ("C-x C-m" . counsel-M-x)
-;;   ("C-x C-f" . counsel-find-file)
-;;   ("C-x c k" . counsel-yank-pop))
-
-;; (use-package counsel-projectile
-;;   :bind
-;;   ("C-x v" . counsel-projectile)
-;;   ("C-x c p" . counsel-projectile-ag)
-;;   :config
-;;   (counsel-projectile-on))
-
-;; (use-package ivy
-;;   :bind
-;;   ("C-x s" . swiper)
-;;   ("C-x C-r" . ivy-resume)
-;;   :config
-;;   (ivy-mode 1)
-;;   (setq ivy-use-virtual-buffers nil
-;;         ivy-re-builders-alist '((read-file-name-internal . ivy--regex-ignore-order)
-;;                                 (projectile-completing-read . ivy--regex-ignore-order)
-;;                                 (counsel-M-x . ivy--regex-ignore-order)
-;;                                 (insert-char . ivy--regex-ignore-order)
-;;                                 (t . ivy--regex-plus)))
-;;   (define-key read-expression-map (kbd "C-r") 'counsel-expression-history))
-
-;; (use-package nerd-icons-ivy-rich
-;;   :init (nerd-icons-ivy-rich-mode 1))
-
-;; (use-package ivy-rich
-;;   :after ivy
-;;   :config
-;;   (ivy-rich-mode 1))
-
 (provide 't--vertico)
